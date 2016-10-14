f58483521e900197d00f691906dddcdc84831709
guilaaf <Mariana@mari-pc>
2016-10-14 00:19:48 -0300

    Criação do date helper e evolução do controller

--- alura-js-avancado-1/client/js/app/controllers/NegociacaoController.js
+++ alura-js-avancado-1/client/js/app/controllers/NegociacaoController.js
@@ -5,17 +5,47 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._tbody = $('table tbody');
         this._negociacoes = [];
     }
     
     adicionarNegociacao(event) {
         event.preventDefault();
         
-        let data = new Date(this._inputData.value.split('-'));
-        let neg = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+        let neg = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
         this._negociacoes.push(neg);
         
-        console.log(this._negociacoes);
+        this._adicionarLinhaTabela(neg);
+        this._limparFormulario();
     }
     
+    _adicionarLinhaTabela(negociacao) {
+        let tr = document.createElement('tr');
+        let td;
+
+        td = document.createElement('td');
+        td.textContent = negociacao.dataFormatada;
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        td.textContent = negociacao.quantidade;
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        td.textContent = negociacao.valor;
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        td.textContent = negociacao.volume;
+        tr.appendChild(td);
+   
+        this._tbody.appendChild(tr);
+    }
+    
+    _limparFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = '1';
+        this._inputValor.value = '0.0';
+        this._inputData.focus();
+    }
 }
--- alura-js-avancado-1/client/js/app/models/Negociacao.js
+++ alura-js-avancado-1/client/js/app/models/Negociacao.js
@@ -1,14 +1,25 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this._data = new Date(data.getTime());
+        this._data = null;
         this._quantidade = quantidade;
         this._valor = valor;
+        
+        if (typeof data === "string") {
+            this._data = DateHelper.text2Date(data);
+        } else if (data instanceof Date) {
+            this._data = new Date(data.getTime());
+        }
+        
         Object.freeze(this);
     }
     
     get data() {
         return new Date(this._data.getTime());
+    }
+    
+    get dataFormatada() {
+        return DateHelper.date2Text(this._data);
     }
 
     get quantidade() {
