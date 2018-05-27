3f6673c4a6c4237e6ca799e62e33039b80acb864
Guilherme <Guilherme@GUILA-PC>
2018-05-27 00:57:54 -0300

    aula 2 do curso de javascript avançado - parte 3

--- alura-js-avancado-3/client/js/app/controllers/NegociacaoController.js
+++ alura-js-avancado-3/client/js/app/controllers/NegociacaoController.js
@@ -20,16 +20,23 @@ class NegociacaoController {
 
         this._ordenacaoAtual = '';
         this._direcaoOrdenacao = 1;
+        
+        new NegociacaoDao().buscarTodos()
+            .then(negociacoes => negociacoes.forEach(neg => this._negociacaoRepository.adicionar(neg)))
+            .catch(msgErro => this._mensagem.texto = msgErro);
     }
     
     adicionarNegociacao(event) {
         event.preventDefault();
         
-        let neg = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
-        this._negociacaoRepository.adicionar(neg);
-        
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._limparFormulario();
+        let neg = new Negociacao(this._inputData.value, parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        new NegociacaoDao().incluir(neg)
+            .then(negociacao => {
+                this._negociacaoRepository.adicionar(negociacao);
+                this._mensagem.texto = 'Negociação adicionada com sucesso';
+                this._limparFormulario();
+            })
+            .catch(msgErro => this._mensagem.texto = msgErro);
     }
     
     importarNegociacoes(event) {
@@ -45,8 +52,13 @@ class NegociacaoController {
     limparNegociacoes(event) {
         event.preventDefault();
         
-        this._negociacaoRepository.esvaziar();
-        this._mensagem.texto = '';
+        new NegociacaoDao().limpar()
+            .then(() => {
+                this._negociacaoRepository.esvaziar();
+                this._mensagem.texto = 'Negociações excluídas com sucesso';
+            })
+            .catch(msgErro => this._mensagem.texto = msgErro);
+        
     }
     
     ordenarNegociacoes(campo) {
